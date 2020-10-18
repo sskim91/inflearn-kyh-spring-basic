b4b9e7f135f6527cc27abdcf00ef1746749195c0
sskim91 <tjdtjq91@gmail.com>
2020-10-18 22:46:25 +0900

    feat: 스프링 데이터 JPA

--- src/main/java/com/inflearn/hellospring/config/SpringConfig.java
+++ src/main/java/com/inflearn/hellospring/config/SpringConfig.java
@@ -2,33 +2,28 @@ package com.inflearn.hellospring.config;
 
 import com.inflearn.hellospring.repository.*;
 import com.inflearn.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
