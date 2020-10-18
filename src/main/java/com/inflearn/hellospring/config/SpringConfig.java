f8d18eeb24308dfe8ef6635a980b0319e56f985d
sskim91 <tjdtjq91@gmail.com>
2020-10-18 21:59:25 +0900

    feat: H2 데이터베이스 설치, 순수 JDBC

--- src/main/java/com/inflearn/hellospring/config/SpringConfig.java
+++ src/main/java/com/inflearn/hellospring/config/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.inflearn.hellospring.config;
 
+import com.inflearn.hellospring.repository.JdbcMemberRepository;
 import com.inflearn.hellospring.repository.MemberRepository;
 import com.inflearn.hellospring.repository.MemoryMemberRepository;
 import com.inflearn.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
