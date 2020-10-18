ed2c849d153e41e79bcfd499fafc8969374562ee
sskim91 <tjdtjq91@gmail.com>
2020-10-18 22:35:19 +0900

    feat: JPA

--- src/main/java/com/inflearn/hellospring/config/SpringConfig.java
+++ src/main/java/com/inflearn/hellospring/config/SpringConfig.java
@@ -1,24 +1,22 @@
 package com.inflearn.hellospring.config;
 
-import com.inflearn.hellospring.repository.JdbcMemberRepository;
-import com.inflearn.hellospring.repository.JdbcTemplateMemberRepository;
-import com.inflearn.hellospring.repository.MemberRepository;
-import com.inflearn.hellospring.repository.MemoryMemberRepository;
+import com.inflearn.hellospring.repository.*;
 import com.inflearn.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,6 +28,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/test/java/com/inflearn/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/inflearn/hellospring/service/MemberServiceIntegrationTest.java
@@ -23,7 +23,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring100");
 
         //when
         Long saveId = memberService.join(member);
